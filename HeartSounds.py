dd52e97d17939d08b1f7c4bd9065cba800b99f6e
cfarley2 <cfarley2@terpmail.umd.edu>
2017-11-11 20:36:50 -0500

    Added making set a labels

--- HeartSounds.py
+++ HeartSounds.py
@@ -1,46 +1,68 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-import wave
-import struct
+import numpy, pandas, wave, struct
 from pylab import *
 from os import listdir
 from os.path import isfile, join
 
-def plot_waves(file):
+def get_raw_data(file):
     FNAME = 'set_a/' + file
     f = wave.open(FNAME)
     frames = f.readframes(-1)
-    print(frames[:20])
-    print(f.getsampwidth())
     samples = struct.unpack('h'*f.getnframes(), frames)
-    print(samples[:10])
+    s = list(samples)
     framerate = f.getframerate()
     t = [float(i)/framerate for i in range(len(samples))]
-    print(t[:10])
-    fig = figure()
-    plot(t, samples)
+    return (t, s)  
 
+def get_training_data(filename):
+    data_frame = pandas.read_csv(filename)
+    return data_frame
 
-mypath = 'set_a/'
-#print(listdir(mypath))
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-#print(onlyfiles)
+def generate_training_labels(df, s1, s2):
+    labels = []
+    for i in range (0, len(df)):
+        if i in s1:
+            labels.append('S1')
+        elif i in s2:
+            labels.append('S2')
+        else:
+            labels.append('none')
+    return labels
 
-for i in range (0,10):
-    plot_waves(onlyfiles[i])
+def get_set_a_data():
+    mypath = 'set_a/'
+    filenames = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and f[-4:] == ".wav")]
+    #filenames = [elem for elem in filenames if elem[-4:] == ".wav"]
+    samples = []
+    times = []
+    
+    
+    for file in filenames:
+        t, s = get_raw_data(file)
+        samples.append(s)
+        times.append(t)
+        
+        #fig = figure()
+        #plot(t, s)
+    return filenames, times, samples
 
-"""
-for file in onlyfiles:
-    plot_waves(file)
+def make_set_a_labels():    
+    training_data = get_training_data('set_a_timing.csv')
+    training_fnames = list(set(training_data["fname"]))
+    s1vals = []
+    s2vals = []
+    training_labels = []
+    
+    
+    for fname in training_fnames:
+        filtered_by_file = training_data.loc[training_data['fname'] == fname]
+        s1 = list(filtered_by_file.loc[filtered_by_file['sound'] == 'S1']['location'])
+        s1vals.append(s1)
+        s2 = list(filtered_by_file.loc[filtered_by_file['sound'] == 'S2']['location'])
+        s2vals.append(s2)
+        
+        fname_index = filenames.index(fname[6:])
+        training_labels.append(generate_training_labels(times[fname_index], s1, s2))
 
-
-print ("blah")
-"""
-
-# In[ ]:
-
-
+filenames, times, samples = get_set_a_data()
+make_set_a_labels()
 
