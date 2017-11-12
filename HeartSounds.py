3baee7a44a4c8b24564edcdd667b3f5280c2b93c
cfarley2 <cfarley2@terpmail.umd.edu>
2017-11-11 21:24:40 -0500

    Added plot_waves_with_labels

--- HeartSounds.py
+++ HeartSounds.py
@@ -2,6 +2,7 @@ import numpy, pandas, wave, struct
 from pylab import *
 from os import listdir
 from os.path import isfile, join
+import matplotlib.pyplot as plt
 
 def get_raw_data(file):
     FNAME = 'set_a/' + file
@@ -31,18 +32,14 @@ def generate_training_labels(df, s1, s2):
 def get_set_a_data():
     mypath = 'set_a/'
     filenames = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and f[-4:] == ".wav")]
-    #filenames = [elem for elem in filenames if elem[-4:] == ".wav"]
     samples = []
     times = []
-    
     
     for file in filenames:
         t, s = get_raw_data(file)
         samples.append(s)
         times.append(t)
         
-        #fig = figure()
-        #plot(t, s)
     return filenames, times, samples
 
 def make_set_a_labels():    
@@ -51,6 +48,7 @@ def make_set_a_labels():
     s1vals = []
     s2vals = []
     training_labels = []
+    training_indices = []
     
     
     for fname in training_fnames:
@@ -61,8 +59,23 @@ def make_set_a_labels():
         s2vals.append(s2)
         
         fname_index = filenames.index(fname[6:])
+        training_indices.append(fname_index)
         training_labels.append(generate_training_labels(times[fname_index], s1, s2))
+    
+    return (s1vals, s2vals, training_labels, training_indices)
+
+def plot_waves_with_labels(t,s,s1,s2):
+    #fig = figure()
+    plt.plot(t, s)
+    for i in range (0, len(s1)):
+        axvline(x = t[s1[i]], color = "red")
+    for i in range (0, len(s2)):
+        axvline(x = t[s2[i]], color = "green")
 
 filenames, times, samples = get_set_a_data()
-make_set_a_labels()
+s1vals, s2vals, training_labels, training_indices = make_set_a_labels()
+
+for i in range(0,1):
+    index = training_indices[i]
+    plot_waves_with_labels(times[index], samples[index], s1vals[i], s2vals[i])
 
